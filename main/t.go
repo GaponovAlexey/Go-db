2e1c5458d6389985be67c7c854ff8f5fc1fc2470
kekelberi <alexei.gaponov@gmail.com>
2022-07-06 17:54:12 +0300

    save

--- main/t.go
+++ main/t.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"conect-db/utils"
 	"fmt"
-
-	"github.com/spf13/viper"
+	// "github.com/fsnotify/fsnotify"
+	// "github.com/spf13/viper"
 )
 
 func main() {
-	vp := viper.New()
-	vp.SetConfigName("test")
-	vp.SetConfigType("json")
-	vp.AddConfigPath(".")
-	err := vp.ReadInConfig()
+	fmt.Println("start")
+
+	config, err := utils.LoadConfig()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println(vp.GetString("foo"))
+	fmt.Println(config)
+
+	// vp := viper.New()
+	// vp.SetConfigName("test")
+	// vp.SetConfigType("json")
+	// vp.AddConfigPath(".")
+	// err := vp.ReadInConfig()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(vp.GetString("foo"))
+
+	// vp.Set("Nama", "Alexeyka")
+	// vp.WriteConfig()
+
+	//fsnotify
+	// watcher, err := fsnotify.NewWatcher()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer watcher.Close()
+	// vp.OnConfigChange(func(in fsnotify.Event) {
+	// 	fmt.Printf("fileChange: %s\n", in.Name)
+	// })
+	// vp.WatchConfig()
+
+	// for {}
 }
---
+++ utils/config.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Dbconfig struct {
+	Address  string `mapstructure:"address"`
+	Port     string `mapstructure:"port"`
+	Password string `mapstructure:"password"`
+	User     string `mapstructure:"user"`
+}
+
+type ServerConfig struct {
+	Port string `mapstructure:"port"`
+}
+
+type Config struct {
+	Db     Dbconfig     `mapstructure:"db"`
+	Server ServerConfig `mapstructure:"server"`
+}
+
+var vp *viper.Viper
+
+func LoadConfig() (Config, error) {
+	vp = viper.New()
+	var config Config
+	vp.SetConfigName("config")
+	vp.SetConfigType("json")
+	vp.AddConfigPath("./utils")
+	vp.AddConfigPath(".")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return Config{}, err
+	}
+	err = vp.Unmarshal(&config)
+	if err != nil {
+		return Config{}, err
+	}
+	return config, nil
+}
