508c3cb4d0f2a6f40630d13f3f99a701c17d08c2
kekelberi <alexei.gaponov@gmail.com>
2022-07-06 21:58:21 +0300

    save

--- main/main.go
+++ main/main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
-func GetDatabase() (*sql.DB, error) {
-	db, err := sql.Open("mysql", os.ExpandEnv("DTS") )
-	return db, err
+type User struct {
+	BookTitle  string `json:"bookTitle"`
+	BookAuthor string `json:"bookAuthor"`
+	BookGenre  string `json:"bookGenre"`
 }
 
 func main() {
-
 	//db
 	db, err := GetDatabase()
 	if err != nil {
@@ -24,13 +26,49 @@ func main() {
 	if err := db.Ping(); err != nil {
 		panic(err)
 	}
+	// type
+
+	// insert, err := db.Query("INSERT INTO `BookSugesstion` (`bookTitle`, `bookAuthor`, `bookGenre`) VALUES('BOOOB', 'MisterBOOOB', 'comedy')")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer insert.Close()
+
+	res, err := db.Query("SELECT `BookTitle`,`bookAuthor`, `bookGenre`  FROM `BookSugesstion`")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for res.Next() {
+		err = res.Scan(&user.BookAuthor, &user.BookTitle, &user.BookGenre)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(fmt.Sprintf("Author: %s with ganre %d with%s", user.BookAuthor, user.BookGenre, user.BookTitle))
+
+	}
 
 	fmt.Println("Successfully connected to PlanetScale!")
 	//page
-	fmt.Println("start")
 	http.HandleFunc("/", HomePage)
 	http.ListenAndServe(":3000", nil)
 }
+
+var user User
+
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "start")
+	fmt.Sprintf("Author: %s with ganre %d with%s", user.BookAuthor, user.BookGenre, user.BookTitle)
+}
+
+func GetDatabase() (*sql.DB, error) {
+	//read
+	err := godotenv.Load() // читать .env
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
+	return db, err
 }
